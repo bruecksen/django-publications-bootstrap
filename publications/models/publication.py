c9281a129df2cbcb26aeaa4a78325aa9287d509e
Christian Glodt <chris@mind.lu>
2017-01-11 10:48:28 +0100

    Use TextField instead of CharField where there is no natural limit to the length of the text. In modern databases (ie. Postgres), there is no performance impact.

--- publications/models/publication.py
+++ publications/models/publication.py
@@ -55,21 +55,19 @@ class Publication(models.Model):
 	type = models.ForeignKey(Type)
 	citekey = models.CharField(max_length=512, blank=True, null=True,
 		help_text='BibTex citation key. Leave blank if unsure.')
-	title = models.CharField(max_length=512)
-	authors = models.CharField(max_length=2048,
-		help_text='List of authors separated by commas or <i>and</i>.')
+	title = models.TextField()
+	authors = models.TextField(help_text='List of authors separated by commas or <i>and</i>.')
 	year = models.PositiveIntegerField()
 	month = models.IntegerField(choices=MONTH_CHOICES, blank=True, null=True)
-	journal = models.CharField(max_length=512, blank=True)
-	book_title = models.CharField(max_length=512, blank=True)
-	publisher = models.CharField(max_length=512, blank=True)
-	institution = models.CharField(max_length=512, blank=True)
+	journal = models.TextField(blank=True)
+	book_title = models.TextField(blank=True)
+	publisher = models.TextField(blank=True)
+	institution = models.TextField(blank=True)
 	volume = models.IntegerField(blank=True, null=True)
 	number = models.IntegerField(blank=True, null=True, verbose_name='Issue number')
 	pages = PagesField(max_length=32, blank=True)
-	note = models.CharField(max_length=512, blank=True)
-	keywords = models.CharField(max_length=512, blank=True,
-		help_text='List of keywords separated by commas.')
+	note = models.TextField(blank=True)
+	keywords = models.TextField(blank=True,	help_text='List of keywords separated by commas.')
 	url = models.URLField(blank=True, verbose_name='URL',
 		help_text='Link to PDF or journal page.')
 	code = models.URLField(blank=True,
